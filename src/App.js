989a89d495d86c4c74db310d9a23d0cb9b77940f
Andrea <rakosfalvyandrea@gmail.com>
2021-08-31 12:20:31 +0200

    button unicode char sorted

--- src/App.js
+++ src/App.js
@@ -13,8 +13,19 @@ function App() {
       <Button className="button" variant="contained"><MenuIcon className="hamburger"></MenuIcon></Button>
       </div>
       <div className="articles">
+        <div className="article">
+          <h2>Emergence of scaling random networks</h2>
+          <h3>A.-L. Barabási, R. Albert - Science 286, 509-512(1999)</h3>
+          <p>Systems as diverse as genetic networks or the World Wide Web are best described as networks with complex topology. A common property of many large networks is that the vertex connectivities follow a scale-free power-law distribution. This feature was found to be a consequence of two generic mechanisms: (i) networks expand continuously by the addition of new vertices, and (ii) new vertices attach preferentially to sites that are already well connected. A model based on these two ingredients reproduces the observed stationary scale-free distributions, which indicates that the development of large networks is governed by robust self-organizing phenomena that go beyond the particulars of the individual systems.</p>
+          <Button className="button" variant="contained">Paper {String.fromCharCode(8226)} PDF</Button>
+        </div>
+        <div className="article">
+          <h2>Emergence of scaling random networks</h2>
+          <h3>A.-L. Barabási, R. Albert - Science 286, 509-512(1999)</h3>
+          <p>Systems as diverse as genetic networks or the World Wide Web are best described as networks with complex topology. A common property of many large networks is that the vertex connectivities follow a scale-free power-law distribution. This feature was found to be a consequence of two generic mechanisms: (i) networks expand continuously by the addition of new vertices, and (ii) new vertices attach preferentially to sites that are already well connected. A model based on these two ingredients reproduces the observed stationary scale-free distributions, which indicates that the development of large networks is governed by robust self-organizing phenomena that go beyond the particulars of the individual systems.</p>
+          <Button className="button" variant="contained">Paper {String.fromCharCode(8226)} PDF</Button>
+        </div>
 
-        <Button className="button" variant="contained">This is my button</Button>
       </div>
     </div>
   );
